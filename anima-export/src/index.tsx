72118341d230fc2dbfa064664c1c2274fb0d1292
abhishekpsa <sam24685615@gmail.com>
2026-04-15 13:27:10 +0530

    remove anima-export folder
    Co-Authored-By: kaamcube

--- anima-export/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { FilterAndSortScreen } from "./screens/FilterAndSortScreen/FilterAndSortScreen";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <FilterAndSortScreen />
-  </StrictMode>,
-);
